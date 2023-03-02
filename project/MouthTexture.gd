58f39a93ee8d2e0e3453f3969a9e406b4c73720a
Edmund <edmundmtang@gmail.com>
2023-03-01 22:39:02 -0500

    Added an FPS counter. Added adaptive calculation of some time-based elements (such as block_size). Updated end_of_sequence behavior to have better alignment. Rearranged method calls inside _physics_process(delta) and _process(delta) to improve performance.

--- project/MouthTexture.gd
+++ project/MouthTexture.gd
@@ -1,36 +1,42 @@
 class_name MouthTexture
 extends Sprite
 
-var upcoming_frames : Array = [[0,0]] # Elements are arrays of pairs of int
+signal end_of_sequence()
+
+var upcoming_frames : Array = [] # Elements are arrays of pairs of int
 var timer : float = 0.0
 var mirror_transitions = false
-var animation_delay : float = 0.5
-var target_delta: float = 0.016667
+var animation_delay : float
+var target_delta: float
 var phoneme_array : Array
 var timings_array : Array
 
 const PHONEME_GROUP : Dictionary = {
 	0 : ["SIL"],
 	1 : ["BB_", "PP_", "MM_"],
-	2 : ["FF_", "VV_", "RR_"],
+	2 : ["FF_", "VV_"],
 	3 : ["YY_", "IY_", "SS_", "SH_", "ZZ_", "ZH_"],
 	4 : ["CH_", "TT_"],
 	5 : ["DD_", "GG_", "KK_", "NN_", "NG_", "JH_", "HH_"],
 	6 : ["TH_", "DH_", "LL_"],
 	7 : ["EH_", "EY_", "IH_"],
 	8 : ["AA_", "AE_", "AH_", "AO_", "AW_", "AY_"],
-	9 : ["UH_", "ER_"],
+	9 : ["UH_", "ER_", "RR_"],
 	10 : ["OY_"],
-	11 : ["WW_", "OW_"],
+	11 : ["WW_", "UW_", "OW_"],
 }
 
-func _ready():
-	pass
+func initialization(delay: float, delta: float) -> void:
+	self.animation_delay = delay
+	self.target_delta = delta
+	assert(self.target_delta > 0, "target_delta must be a positive value")
+	assert(self.animation_delay > self.target_delta, "animation_delay must be a positive value greater than target_delta")
 
-func reset_pointer():
-	timer = 0 - animation_delay
+func update_texture(phoneme_labels: Array, phoneme_timings: Array, delta: float) -> void:
+	add_next_frame(phoneme_labels, phoneme_timings)
+	update_frame(delta)
 
-func add_next_texture(phoneme_labels: Array, phoneme_timings: Array) -> void:
+func add_next_frame(phoneme_labels: Array, phoneme_timings: Array) -> void:
 	# Queues the next texture in the array of upcoming frames
 	phoneme_array = phoneme_labels
 	timings_array = phoneme_timings
@@ -45,7 +51,7 @@ func get_label_index_triple() -> Array:
 	var reference_time := timer
 	var j = 0
 	for _i in range(3):
-		if timer < 0:
+		if reference_time < 0:
 			result.append(-1)
 		else:
 			while j < timings_array.size():
@@ -55,8 +61,8 @@ func get_label_index_triple() -> Array:
 				j += 1
 			if j >= timings_array.size():
 				result.append(-1)
-				# To-Do: We're going to need to figure out how to reach into the next segment at this point
-		reference_time += target_delta # target_delta might need to be true delta depending on how lag/time updates interact
+				emit_signal("end_of_sequence")
+		reference_time += target_delta
 	return result
 
 func index_triple_to_state_triple(triple: Array) -> Array:
@@ -84,7 +90,6 @@ func analyze_for_frame(triple: Array) -> Array:
 	else:
 		return [triple[1], triple[1]]
 
-
 func add_frame(frame_coords: Array):
 	# Adds a frame to the queue of upcoming frames
 	var new_frame := frame_coords
@@ -93,6 +98,6 @@ func add_frame(frame_coords: Array):
 	upcoming_frames.push_back(new_frame)
 
 func update_frame(delta) -> void:
-	timer += delta
 	var next_frame_coords : Array = upcoming_frames.pop_front()
 	self.frame_coords = Vector2(next_frame_coords[0], next_frame_coords[1])
+	timer += delta
--- project/main.gd
+++ project/main.gd
@@ -2,21 +2,33 @@ extends Node2D
 
 onready var recording_player = $Recording_Player
 onready var mouth_texture = $MouthTexture
-
+onready var fps_label = $FPSLabel
 
 var VoskRecognizerSource = load("res://bin/gdvosk.gdns")
 var voice_recognizer = VoskRecognizerSource.new()
 var audio_mix_rate: float = ProjectSettings.get_setting("audio/mix_rate")
-var block_size: int = 1600
+var physics_fps: float = ProjectSettings.get_setting("physics/common/physics_fps")
+var force_fps: float = ProjectSettings.get_setting("debug/settings/fps/force_fps")
+
+var result_option := "phones"
+var verbose_option := false
 
 var buffer = StreamPeerBuffer.new()
+var timer : float = 0.0
+var force_delta: float = 1.0/30
+var animation_delay : float = 0.5
+var block_size : int = 1600
+var accept_status := 01
 
-var phoneme_labels: Array = ["SIL"]
-var phoneme_timings: Array = [0.0]
-var phoneme_starts: Array = [0.0]
-var text_result: String = ""
+var frame_counter : int = 0
+var delta_total : float = 0
 
-var is_in_segment_transition := false
+var phoneme_labels : Array = ["SIL"]
+var phoneme_timings : Array = [0.0]
+var phoneme_starts : Array = [0.0]
+var text_result : String = ""
+
+var is_in_between_sequences := false
 
 var lm: Dictionary = {
 	small_en = "language_models/vosk-model-small-en-us-0.15"
@@ -24,23 +36,31 @@ var lm: Dictionary = {
 
 func _ready():
 	print("Starting Test!")
-
-	voice_recognizer.initialize(
-		lm.small_en, # model_path
-		48000 # sample rate
-		)
-	var result_option := "phones"
-	voice_recognizer.set_result_options("phones")
-	voice_recognizer.set_verbose(false)
+	initialize_parameters()
+	mouth_texture.initialization(animation_delay, force_delta)
+	initialize_voice_recognizer()
 
 	assert($Test_HUD.connect("record_toggled", self, "_on_record_button_toggle") == 0)
+	assert(mouth_texture.connect("end_of_sequence", self, "align_timers") == 0)
 
 	print("Model path: ", voice_recognizer.model_path)
 	print("Sample rate: ", voice_recognizer.sample_rate)
 	print("Buffer_size: ", voice_recognizer.buffer_size())
+	print("Effective block size: ", block_size * 2)
 	print("Result option: ", result_option)
-
 	print('End of Test Initialization\n')
+
+func initialize_parameters() -> void:
+	self.block_size = audio_mix_rate / physics_fps * 2
+	self.force_delta = 1 / force_fps
+
+func initialize_voice_recognizer() -> void:
+	voice_recognizer.initialize(
+		lm.small_en, # model_path
+		audio_mix_rate # sample rate
+		)
+	voice_recognizer.set_result_options(result_option)
+	voice_recognizer.set_verbose(verbose_option)
 
 func _on_record_button_toggle() -> void:
 	if recording_player.is_recording:
@@ -51,41 +71,53 @@ func _on_record_button_toggle() -> void:
 func start_recording() -> void:
 	print("Microphone on")
 	recording_player.start_recording()
-	mouth_texture.reset_pointer()
+	full_reset_timer()
+	align_timers()
 
 func stop_recording() -> void:
 	print("Microphone off")
 	recording_player.stop_recording()
 
-	var final_output = voice_recognizer.get_final()
-	print(final_output)
-	#print(JSON.parse(final_output).get_result()["text"])
+func align_timers() -> void:
+	mouth_texture.timer = timer
+	is_in_between_sequences = false
 
-func _physics_process(_delta):
+func full_reset_timer() -> void:
+	timer = 0 - animation_delay - force_delta
+
+func _physics_process(delta):
 	if recording_player.is_recording and (recording_player.get_frames_available() >= block_size):
-		var accept_status: int = pass_audio_data()
+		accept_status = pass_audio_data()
+	timer += delta
 
+func pass_audio_data() -> int:
+	var sub_buffer = recording_player.get_buffer(block_size)
+	return voice_recognizer.accept_waveform(sub_buffer)
+
+func _process(delta):
+	update_fps_label(delta)
+	if recording_player.is_recording:
 		if accept_status == 0:
 			text_result = JSON.parse(voice_recognizer.get_partial()).result.partial
 		elif accept_status == 1:
 			text_result = JSON.parse(voice_recognizer.get_final()).result.text
-			is_in_segment_transition = true
-		else:
-			assert(false, "Something went wrong with voice_recognizer.accept_waveform().")
+			full_reset_timer()
+			is_in_between_sequences = true
+			print(text_result)
 
-		var phone_output: Dictionary
-		phone_output = JSON.parse(voice_recognizer.get_phone_partial()).result
+		if !is_in_between_sequences:
+			var phone_output: Dictionary
+			phone_output = JSON.parse(voice_recognizer.get_phone_partial()).result
+			phoneme_labels = phone_output.phone_labels
+			phoneme_timings = phone_output.phone_end
 
-		#if !is_in_segment_transition:
-		phoneme_labels = phone_output.phone_labels
-		phoneme_timings = phone_output.phone_end
-		phoneme_starts = phone_output.phone_start
+	# Texture updates
+	mouth_texture.update_texture(phoneme_labels, phoneme_timings, delta)
 
-func pass_audio_data() -> int:
-	var sub_buffer = recording_player.get_buffer(block_size)
-
-	return voice_recognizer.accept_waveform(sub_buffer)
-
-func _process(delta):
-	mouth_texture.add_next_texture(phoneme_labels, phoneme_timings)
-	mouth_texture.update_frame(delta)
+func update_fps_label(delta) -> void:
+	frame_counter += 1
+	delta_total += delta
+	if frame_counter == 10:
+		fps_label.text = str(frame_counter/delta_total).pad_decimals(1)
+		frame_counter = 0
+		delta_total = 0
